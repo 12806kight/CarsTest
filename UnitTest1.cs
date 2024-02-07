f0eb26d6194176e496f7b326bc937ac42346a492
Brewer <12806kight@gmail.com>
2024-02-06 18:25:48 -0600

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace CarsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, 10, .001);
+        }
+    }
+}
